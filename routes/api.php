1d5961e5120a2868dae99091fb47a3c2bbcf4896
Alvaro Molina Jara <22986107+AlvaroMolinaCL@users.noreply.github.com>
2026-06-02 21:25:24 -0400

    Se actualiza routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,11 @@
 
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\EventController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-Route::apiResource('categories', CategoryController::class)->only(['index', 'store', 'show']);
-Route::apiResource('events', EventController::class)->only(['index', 'store', 'show']);
+Route::apiResource('categories', CategoryController::class)->only(['index', 'show', 'store']);
+Route::apiResource('events', EventController::class)->only(['index', 'show', 'store']);
